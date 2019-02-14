ea74e63d0e79d118b787b0b5263332c93eca7f4c
Sebastien Besson <seb.besson@gmail.com>
2019-02-14 21:17:09 +0000

    Migrate Molecule v1 to Molecule v2

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,9 +6,16 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_services_running_and_enabled(host):
+    assert host.service('prometheus-omero-exporter').is_running
+    assert host.service('prometheus-omero-exporter').is_enabled
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+
+@pytest.mark.parametrize("metric", [
+    'omero_sessions_processing_seconds_sum',
+    'omero_sessions_active',
+    'omero_counts_processing_seconds_sum',
+])
+def test_node_exporter_metrics(Command, metric):
+    out = host.check_output('curl http://localhost:9449/metrics')
+    assert metric in out
